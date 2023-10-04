206057eae392fa0ca698bcc7c610fa6aaa561b97
Vnuchara <99031976+Vnuchara@users.noreply.github.com>
2023-10-04 19:59:02 +0300

    3.9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyArrayList employees = getEmployees();
+        employees.add("LOX");
+        System.out.println(employees.getSize());
+//        employees.remove("John");
+        for (int i = 0; i < employees.getSize(); i++) {
+            System.out.println(employees.get(i));
+        }
+    }
+
+    private static MyArrayList getEmployees() {
+        MyArrayList employees = new MyArrayList();
+        employees.add("John");
+        employees.add("Lol");
+        employees.add("kek");
+        return employees;
     }
 }
